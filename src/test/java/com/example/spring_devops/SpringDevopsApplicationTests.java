416188559d3e0aa50335424c7052af75c9755702
umar <rusumumarfarooq@gmail.com>
2026-06-02 16:14:51 +0530

    Initial commit: Brand new Spring Boot sandbox project

---
+++ src/test/java/com/example/spring_devops/SpringDevopsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring_devops;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDevopsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
